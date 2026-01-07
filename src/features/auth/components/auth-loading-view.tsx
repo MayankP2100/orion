0bd16b0ed456d6206e0cd2afcf861080582078e2
Mayank Pal <palmayank2100@gmail.com>
2026-01-07 15:32:22 +0530

    Integrate convex with authentication

---
+++ src/features/auth/components/auth-loading-view.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Spinner } from '@/components/ui/spinner';
+
+export const AuthLoadingView = () => {
+  return (
+    <div className='flex h-screen items-center justify-center bg-background'>
+      <Spinner className='size-6 text-ring' />
+    </div>
+  );
+};
